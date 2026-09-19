a8309b245f323bc5ed9cf06c2b0fe7b3422021f2
balu <balugollapalli456@gmail.com>
2026-09-19 13:19:49 +0530

    Initial Terraform configuration

---
+++ main.tf
@@ -0,0 +1,103 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "demo" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+resource "azurerm_virtual_network" "demo" {
+  name                = var.vnet_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.demo.name
+  address_space       = var.address_space
+}
+
+resource "azurerm_subnet" "demo" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.demo.name
+  virtual_network_name = azurerm_virtual_network.demo.name
+  address_prefixes     = var.subnet_address_prefixes
+}
+
+resource "azurerm_network_security_group" "demo" {
+  name                = var.nsg_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.demo.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix     = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "demo" {
+  subnet_id                 = azurerm_subnet.demo.id
+  network_security_group_id = azurerm_network_security_group.demo.id
+}
+
+resource "azurerm_public_ip" "demo" {
+  name                = var.public_ip_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.demo.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_interface" "demo" {
+  name                = var.nic_name
+  location            = var.location
+  resource_group_name = azurerm_resource_group.demo.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.demo.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.demo.id
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "demo" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.demo.name
+  location            = var.location
+  size                = var.vm_size
+
+  admin_username = var.admin_username
+  admin_password = var.admin_password
+
+  disable_password_authentication = false
+
+  network_interface_ids = [
+    azurerm_network_interface.demo.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "ubuntu-24_04-lts"
+    sku       = "server"
+    version   = "latest"
+  }
+}
---
+++ varables.tf
@@ -0,0 +1,52 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "vnet_name" {
+  type = string
+}
+
+variable "address_space" {
+  type = list(string)
+}
+
+variable "subnet_name" {
+  type = string
+}
+
+variable "subnet_address_prefixes" {
+  type = list(string)
+}
+
+variable "nsg_name" {
+  type = string
+}
+
+variable "public_ip_name" {
+  type = string
+}
+
+variable "nic_name" {
+  type = string
+}
+
+variable "vm_name" {
+  type = string
+}
+
+variable "vm_size" {
+  type = string
+}
+
+variable "admin_username" {
+  type = string
+}
+
+variable "admin_password" {
+  type      = string
+  sensitive = true
+}
