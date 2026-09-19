66b3176a41fb6c6bf94177b7fb4e5f8c13e9845c
balu <balugollapalli456@gmail.com>
2026-09-19 16:31:18 +0530

    dded

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "azurerm_network_security_group" "demo" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
-    source_address_prefix     = "*"
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 }
